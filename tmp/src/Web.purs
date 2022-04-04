5cd564bf68677b6d85c0b7c6c930fd83b06cb829
hbybyyang <2451759073@qq.com>
2022-04-05 04:57:04 +0800

    .

--- tmp/src/Web.purs
+++ tmp/src/Web.purs
@@ -1,27 +1,31 @@
 module Web where
 
 import Prelude
+import Data.Lens (set)
 import Effect (Effect)
 import Hby.React.Grid (GridItemArea(..), GridSize(..))
 import Hby.Task (Task, runTask_)
 import Model.Display.Screen (Screen(..), addItem, mkScreenItem)
 import Model.Display.Screen (render) as S
-import Model.UI.Counter (Counter(..))
+import Model.UI.Counter (Counter(..), _n)
 import Model.UI.CounterF (CounterF(..))
 import Model.UI.TestButton (TestButton(..))
 import Model.UI.TestElement (TestElement(..))
 
+type State
+  = { n :: Number }
+
 main :: Effect Unit
 main = runTask_ $ render { n: 0.0 }
   where
-  render :: { n :: Number } -> Task Unit
-  render { n } = do
+  render :: State -> Task Unit
+  render s@{ n } = do
     item <-
       pure $ identity
-        $ addItem { area: GridItemArea 0 0 1 1, obj: Counter { n, addEvent: \n' -> render { n: n' } } }
-        $ addItem { area: GridItemArea 1 0 2 1, obj: Counter { n, addEvent: \n' -> render { n: n' } } }
+        $ addItem { area: GridItemArea 0 0 1 1, obj: Counter { n, addEvent: render <<< onAdd s } }
+        $ addItem { area: GridItemArea 1 0 2 1, obj: Counter { n, addEvent: render <<< onAdd s } }
         $ addItem { area: GridItemArea 0 1 1 1, obj: TestElement unit }
-        $ addItem { area: GridItemArea 1 1 2 2, obj: CounterF { n, onClick: \n' -> render { n: n' } } }
+        $ addItem { area: GridItemArea 1 1 2 2, obj: CounterF { n, onClick: render <<< onAdd s } }
         $ addItem { area: GridItemArea 0 2 2 3, obj: TestButton unit }
         $ mkScreenItem
     S.render
@@ -30,3 +34,6 @@ main = runTask_ $ render { n: 0.0 }
           , colSize: [ GridSize_Fr 1, GridSize_Fr 1 ]
           , item: item
           }
+
+  onAdd :: State -> Number -> State
+  onAdd s n' = set _n n' s
