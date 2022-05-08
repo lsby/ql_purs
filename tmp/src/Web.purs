f2b0847f2ee9321f972c29cefbc5a7a12261cb18
hbybyyang <2451759073@qq.com>
2022-05-08 18:15:06 +0800

    加两个例子

--- tmp/src/Web.purs
+++ tmp/src/Web.purs
@@ -5,7 +5,9 @@ import Component.Clock as Clock
 import Component.Counter2 as Counter2
 import Component.FFI.Button as FFI
 import Component.Hello as Hello
+import Component.Loop as Loop
 import Component.Signal as Signal
+import Component.State as State
 import Concur.React.DOM as D
 import Concur.React.Run (runWidgetInDom)
 import Effect (Effect)
@@ -21,6 +23,10 @@ main =
         , D.div' [ Signal.main ]
         , D.div' [ D.text "=======" ]
         , D.div' [ Counter2.main ]
+        , D.div' [ D.text "=======" ]
+        , D.div' [ Loop.main 0 ]
+        , D.div' [ D.text "=======" ]
+        , D.div' [ State.main { a: 0, b: 0 } ]
         , D.div' [ D.text "=======" ]
         , D.div' [ FFI.button [] [ D.text "组件按钮" ] ]
         ]
