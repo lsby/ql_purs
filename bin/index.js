f090643a8b4c257bf0244575f5b0e366877a0654
hbybyyang <2451759073@qq.com>
2022-07-03 10:03:36 +0800

    .

--- bin/index.js
+++ bin/index.js
@@ -2,17 +2,20 @@
 
 var exec = require("@lsby/exec_cmd").default;
 var path = require("path");
+var download = require("download");
 
 async function main() {
   var 命令所在目录 = process.cwd();
-  var 临时目录 = path.resolve(命令所在目录, "tmp_ql_ts");
-  await exec(`mkdir ${临时目录}`);
-  await exec(
-    `git clone --depth 1 https://github.com/lsby/Template_Purescript.git ${临时目录}`
+  var 临时目录 = path.resolve(命令所在目录, "Template_Purescript-main");
+
+  await download(
+    "https://github.com/lsby/Template_Purescript/archive/refs/heads/main.zip",
+    命令所在目录,
+    { extract: true }
   );
-  await exec(`rm -rf ${临时目录}/.git`);
   await exec(`mv ${临时目录}/{.[!.],}* ${命令所在目录}/`);
   await exec(`rm -rf ${临时目录}`);
+
   console.log("安装npm依赖:", "npm i");
   console.log("编译purs:", "spago build");
   console.log("监视编译&运行前端:", "F1 -> 运行任务 -> watch:all");
